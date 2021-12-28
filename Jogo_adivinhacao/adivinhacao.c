03525b490ced110c1ae1a188cc0d6e5a442b8da8
luizferreiira <luizferreiraa34@gmail.com>
2021-12-28 17:43:50 -0300

    Jogo_adivinhacao/

--- Jogo_adivinhacao/adivinhacao.c
+++ Jogo_adivinhacao/adivinhacao.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define NUMERO_TENTATIVAS 5
+//#define NUMERO_TENTATIVAS 5
 
 //imprime cabeçalho do nosso jogo
 int main(){
@@ -11,29 +11,30 @@ int main(){
     int numero_secreto = 42;
 
     int chute;
+    int ganhou = 0;
+    int tentativas = 1;
 
-    for(int i = 1; i <= NUMERO_TENTATIVAS; i++){
+    //for(int i = 1; i <= NUMERO_TENTATIVAS; i++){
+    while(ganhou == 0){
 
-        printf("Tentativa %d de %d \n",i,NUMERO_TENTATIVAS);
+        printf("Tentativa %d \n",tentativas);
         printf("Qual é o seu chute? ");
         scanf("%d",&chute);
         printf("Seu chute foi: %d \n",chute);
 
         if(chute < 0){
             printf("Você não pode chutar números negativos \n");
-            i--;
-
             continue;
         }
 
         int acertou = (chute == numero_secreto);
         int maior = (chute > numero_secreto);
-        int menor = (chute < numero_secreto);
 
         if(acertou){
             printf("Parabéns, você acertou! \n");
             printf("Jogue de novo, você é um bom jogador! \n");
-            break;
+            //break;
+            ganhou = 1; // posso usar o break tbm
         }
 
         else if(maior){
@@ -43,7 +44,10 @@ int main(){
         else{
             printf("Seu chute foi menor que o número secreto \n");
         }
+
+        tentativas++;
     }
     printf("Fim de jogo \n");
+    printf("Você acertou em %d tentativas! \n ",tentativas-1);
     
 } // PAREI EM "MAIS REFATORAÇÕES" OBS: IGNORE ESSE COMENTÁRIO.
